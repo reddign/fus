8ce15c011ebf149c204064cba86444355f6ba529
AidanHanley <hanleya@etown.edu>
2022-09-29 09:40:42 -0400

    basic login page

--- src/FUSlogin.php
+++ src/FUSlogin.php
@@ -7,6 +7,9 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div id="login_fields">
+        Username: <input type="text" name="username">
+        Password: <input type="text" name="password">
+    </div>
 </body>
 </html>
