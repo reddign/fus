8a4fd86a5078943db0df79a681745a662397ca21
Seamus Corr <corrs@etown.edu>
2022-10-13 10:22:14 -0400

    basic homepage css

--- src/FUShome.php
+++ src/FUShome.php
@@ -5,13 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="FUShome.css">
 </head>
 <body>
     <div class="header">
-        <a href="FUSlogin.php">Log in</a><br>
-        <a href="FUSeventsignup.php">Sign up</a> for an event<br>
-        See an event <a href="FUScalendar.php">calendar</a><br>
-        Check <a href="FUSpricing.php">pricing</a> options<br>
+        <h1>Facility Use System</h1>
+    </div>
+    <div class="links">
+        <h2><a href="FUSlogin.php">Log in</a><br></h2>
+        <h2><a href="FUSeventsignup.php">Sign up</a> for an event<br></h2>
+        <h2> See an event <a href="FUScalendar.php">calendar</a><br></h2>
+        <h2>Check <a href="FUSpricing.php">pricing</a> options<br></h2>
     </div>
 </body>
 </html>
