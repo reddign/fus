d578fa1a866ac0f4c4d61a26403b5590a3c878ff
Seamus Corr <corrs@etown.edu>
2022-10-20 09:16:01 -0400

    set events html and css, added dropdown

--- src/FUSeventsignup.php
+++ src/FUSeventsignup.php
@@ -2,13 +2,26 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Event Sign Up</title>
+    <link rel="stylesheet" href="FUSeventsignup.css">
+    <script src="FUSeventsignup.js"></script>
 </head>
 <body>
+    <div class="header">
+        <h1>Event Sign Up</h1>
+    </div>
     <form>
         <!-- todo: add organization dropdown selector -->
+        <div class="dropdown">
+            <button onclick="myFunction()" class="dropbtn">Organization</button>
+            <div id="myDropdown" class="dropdown-content">
+                <a href="">Faculty</a>
+                <a href="">Student</a>
+                <a href="">Local Sports Team</a>
+                <a href="">Other</a>
+            </div>
+        </div>
 
         <!-- todo: find necessary fields to fill in form -->
 
