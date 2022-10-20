eb3054d12c387c3c711b30d63babc9d8e7c2b7de
AidanHanley <hanleya@etown.edu>
2022-10-20 08:45:27 -0400

    added needed fields for account creation

--- src/FUSregistration.php
+++ src/FUSregistration.php
@@ -7,10 +7,19 @@
     <title>Document</title>
 </head>
 <body>
-    <form>
+    <form method="post" action="functions/register.php">
         <label for="username">Username:</label> <input type="text" name="username"> <br>
         <label for="password">Password:</label> <input type="password" name="password"> <br>
-        <label for="password">Confirm Password:</label> <input type="password" name="c_password"> <br>
+        <label for="c_password">Confirm Password:</label> <input type="password" name="c_password"> <br>
+
+        <label for="first_name">Password:</label> <input type="text" name="first_name"> <br>
+        <label for="last_name">Password:</label> <input type="text" name="last_name"> <br>
+        
+        <label for="address">Password:</label> <input type="text" name="address"> <br>
+        <label for="city">Password:</label> <input type="text" name="city"> <br>
+        <label for="state">Password:</label> <input type="text" name="state"> <br>
+        <label for="zip">Password:</label> <input type="text" name="zip"> <br>
+        <label for="phone">Password:</label> <input type="text" name="phone"> <br>
 
         <!-- find other necessary info for registration -->
 
