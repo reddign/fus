8441602de30d2df6355a12288e8eb1fc2a64190f
AidanHanley <hanleya@etown.edu>
2022-10-20 09:01:17 -0400

    register function inserts into user_outside

--- src/functions/db_functions.php
+++ src/functions/db_functions.php
@@ -28,4 +28,15 @@
 
         return false;
     }
+
+
+    function registerUser($u, $p, $cp, $fn, $ln, $ad, $ct, $st, $zp, $ph) {
+        $query = "INSERT INTO user_outside (username, password, approved, first_name, last_name, address, city, state, zip, phone) 
+            VALUES (:u, :p, 0, :fn, :ln, :ad, :ct, :st, :zp, :ph);";
+        $params = [":u" => $u, ":p" => $p, ":fn" => $fn, ":ln" => $ln, ":ad" => $ad, ":ct" => $ct, ":st" => $st, ":zp" => $zp, ":ph" => $ph];
+
+        $db = connect();
+        $stmt = $db->prepare($query);
+        $stmt->execute($params);
+    }
 ?>
---
+++ src/functions/register.php
@@ -0,0 +1,20 @@
+<?php
+    require "db_functions.php";
+    session_start();
+    
+    $username = isset($_POST)?$_POST["username"]:"";
+    $password = isset($_POST)?$_POST["password"]:"";
+    $c_password = isset($_POST)?$_POST["c_password"]:"";
+
+    $first_name = isset($_POST)?$_POST["first_name"]:"";
+    $last_name = isset($_POST)?$_POST["last_name"]:"";
+
+    $address = isset($_POST)?$_POST["address"]:"";
+    $city = isset($_POST)?$_POST["city"]:"";
+    $state = isset($_POST)?$_POST["state"]:"";
+    $zip = isset($_POST)?$_POST["zip"]:"";
+    $phone = isset($_POST)?$_POST["phone"]:"";
+    
+    registerUser($username, $password, $c_password, $first_name, $last_name, $address, $city, $state, $zip, $phone);
+
+?>
