b40b0bcef130ec1aa2565dd7ccd907a89d3cbb51
AidanHanley <hanleya@etown.edu>
2022-11-03 10:22:28 -0400

    added function to check for conflicts with event

--- src/FUSreqapproval.php
+++ src/FUSreqapproval.php
@@ -1,3 +1,9 @@
+<?php
+    require "functions/db_functions.php";
+
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,5 +14,20 @@
 </head>
 <body>
     
+<?php include "FUSnavbar.php"?>
+    <form method="post" action="functions/approve_events.php">
+    <?php
+    /*
+        $users = getUnapprovedRequests();
+        foreach($users as $u) { 
+            echo "<input type='checkbox' name='"."approve_".$u["username"]."' value=".$u["user_id"]."> ";
+            echo "<label for='"."approve_".$u["username"]."'>";
+            echo $u["last_name"].", ".$u["first_name"];
+            echo "</label><br>";
+        }
+        */
+    ?><br>
+    <input type="submit" id="approve" value="Approve"></input>
+    </form>
 </body>
 </html>
--- src/functions/db_functions.php
+++ src/functions/db_functions.php
@@ -87,4 +87,108 @@
             $stmt->execute($params);
         }
     }
+
+    function getPendingRequests() {
+        $query = "SELECT * FROM request WHERE processed = 0;";
+
+        $db = connect();
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        
+        $requests = $stmt->fetchAll();
+
+        return $requests;
+    }
+
+    function getEvents($request_id) {
+        $query = "SELECT * FROM event WHERE request_id = :rid ORDER BY start_time;";
+        $params = [":rid" => $request_id];
+
+        $db = connect();
+        $stmt = $db->prepare($query);
+        $stmt->execute($params);
+
+        $events = $stmt->fetchAll();
+
+        return $events;
+    }
+
+    function getLocation($lid) {
+        $query = "SELECT * FROM location WHERE loc_id = :lid;";
+        $params = [":lid" => $lid];
+
+        $db = connect();
+        $stmt = $db->prepare($query);
+        $stmt->execute($params);
+
+        $loc = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $loc;
+    }
+
+    function getUser($uid) {
+        $query = "SELECT * FROM user_outside WHERE user_id = :uid;";
+        $params = [":uid" => $uid];
+
+        $db = connect();
+        $stmt = $db->prepare($query);
+        $stmt->execute($params);
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $user;
+    }
+
+    function getEvent($eid) {
+        $query = "SELECT * FROM event WHERE event_id = :eid;";
+        $params = [":eid" => $eid];
+
+        $db = connect();
+        $stmt = $db->prepare($query);
+        $stmt->execute($params);
+
+        $event = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $event;
+    }
+    
+    function getRequest($rid) {
+        $query = "SELECT * FROM request WHERE request_id = :rid;";
+        $params = [":rid" => $rid];
+
+        $db = connect();
+        $stmt = $db->prepare($query);
+        $stmt->execute($params);
+
+        $request = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $request;
+    }
+
+    function checkConflict($event_id) {
+
+        $event = getEvent($event_id);
+        $request = getRequest($event["request_id"]);
+
+        $query = "  SELECT e.event_id
+                    FROM event e
+                        JOIN request r ON (e.request_id = r.request_id)
+                    WHERE r.loc IN (
+                            SELECT loc_2
+                            FROM location_dependency
+                            WHERE loc_1 = :loc
+                        ) AND ((e.start_date BETWEEN :start AND :end)
+                            OR (e.end_date BETWEEN :start AND :end)
+                            OR (:start BETWEEN e.start_date AND e.end_date));";
+        $params = [":loc" => $request["loc_id"], ":start" => $event["start_date"], ":end" => $event["end_date"]];
+
+        $db = connect();
+        $stmt = $db->prepare($query);
+        $stmt->execute($params);
+
+        $events = $stmt->fetchAll();
+
+        return $events;
+
+    }
 ?>
