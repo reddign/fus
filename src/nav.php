35d4db096cea9be4bde262504989a2b554421542
Seamus Corr <corrs@etown.edu>
2022-11-03 09:12:04 -0400

    created nav bar

---
+++ src/nav.php
@@ -0,0 +1,38 @@
+<nav>
+    <div id="nav">
+        <li><a href="FUShome.php">Home</a></li>
+        <li><a href="FUSeventsignup.php">Event Sign Up</a></li>
+        <li><a href="FUScalendar.php">Event Calander</a></li>
+        <li><a href="FUSpricing.php">Pricing</a></li>
+    </div>
+</nav>
+
+<style>
+    #nav{
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #666;
+        top: 0;
+        width: 100%;
+        margin-bottom: 45px;
+    }
+    
+    li {
+        float: left;
+        border-right: 1px solid #bbb;
+    }
+
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    li a:hover {
+        background-color: #111;
+    }
+</style>
