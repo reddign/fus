b9df3c4076ff4bebb3f8ce54e549d06aedc7f1de
Seamus Corr <corrs@etown.edu>
2022-11-17 08:49:16 -0500

    end date, and drop downs

--- src/FUSeventsignup.php
+++ src/FUSeventsignup.php
@@ -28,8 +28,10 @@
             <button onclick="Function()" type="button" class="dropbtn">Organization</button>
             <div id="Dropdown" class="dropdown-content">
                 <a href="">Faculty</a>
-                <a href="">Student</a>
                 <a href="">Local Sports Team</a>
+                <a href="">Local Group</a>
+                <a href="">Student</a>
+                <a href="">School Sports Team</a>
                 <a href="">Other</a>
             </div>
         </div>
@@ -41,35 +43,29 @@
         <div class="dropdown">
             <button onclick="Function2()" type="button" class="dropbtn drop">Location</button>
             <div id="Dropdown2" class="dropdown-content">
-                <a href="">Faculty</a>
-                <a href="">Student</a>
-                <a href="">Local Sports Team</a>
-                <a href="">Other</a>
+                <a href="">Auditorium</a>
+                <a href="">Cafeteria</a>
+                <a href="">Conferance room 1</a>
+                <a href="">Conferance room 2</a>
+                <a href="">Football Field</a>
+                <a href="">Gym</a>
+                <a href="">Soccer Field</a>                
             </div>
         </div>
         <br>
-        <div class="dropdown">
-            <button onclick="Function3()" type="button" class="dropbtn drop">Area</button>
-            <div id="Dropdown3" class="dropdown-content">
-                <a href="">Faculty</a>
-                <a href="">Student</a>
-                <a href="">Local Sports Team</a>
-                <a href="">Other</a>
-            </div>
-        </div>
+        <br>
         <!-- todo: add date/time selectors (link to calendar?) -->
-        <p>Select date and time: <br><input type="datetime-local"/></p> 
+        <p>Select date and start time: <br><input type="datetime-local"/></p>
+        <p>Select end time: <br><input type="time"/></p> 
 
         Repeating Event<input type="checkbox" name="Repeat" id="check"><br>
         <div id="radio">
-        How often does this event repeat: <br>
-        Daily<input type="radio" name="repeat">
-        Weekly<input type="radio" name="repeat">
-        Monthly<input type="radio" name="repeat"> 
-        Biweekly<input type="radio" name="repeat">
-        Other<input type="radio" name="repeat"><br>
-
-        <p>Select end date: <br><input type="date"/></p>
+            How often does this event repeat: <br>
+            Daily<input type="radio" name="repeat">
+            Weekly<input type="radio" name="repeat">
+            Biweekly<input type="radio" name="repeat">
+            Monthly<input type="radio" name="repeat"> 
+            <p>Select end date: <br><input type="date"/></p>
         </div>
 
         <input type="submit" value="Submit">
