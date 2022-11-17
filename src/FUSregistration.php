8125d43f1fda4b43d5e112bf29aaa5b932e972a9
AidanHanley <hanleya@etown.edu>
2022-11-17 09:37:21 -0500

    added confirm password check

--- src/FUSregistration.php
+++ src/FUSregistration.php
@@ -9,24 +9,25 @@
 <body>
     <?php include "FUSnavbar.php"?>
     <form method="post" action="functions/register.php">
-        <label for="username">Username:</label> <input type="text" name="username"> <br>
-        <label for="password">Password:</label> <input type="password" name="password"> <br>
-        <label for="c_password">Confirm Password:</label> <input type="password" name="c_password"> <br>
+        <label for="username">Username:</label> <input type="text" name="username" required> <br>
+        <label for="password">Password:</label> <input type="password" name="password" id="password" onchange="form.c_password.pattern = this.value;" required> <br>
+        <label for="c_password">Confirm Password:</label> <input type="password" name="c_password" id="c_password" onchange="this.setCustomValidity(this.validity.patternMismatch ? 'Please enter the same Password as above' : '');" required> <br>
         <br>
-        <label for="first_name">First Name:</label> <input type="text" name="first_name"> <br>
-        <label for="last_name">Last Name:</label> <input type="text" name="last_name"> <br>
+        <label for="first_name">First Name:</label> <input type="text" name="first_name" required> <br>
+        <label for="last_name">Last Name:</label> <input type="text" name="last_name" required> <br>
         <br>
-        <label for="address">Address:</label> <input type="text" name="address"> <br>
-        <label for="city">City:</label> <input type="text" name="city"> <br>
-        <label for="state">State:</label> <input type="text" name="state"> <br>
-        <label for="zip">Zip Code:</label> <input type="text" name="zip"> <br>
+        <label for="address">Address:</label> <input type="text" name="address" required> <br>
+        <label for="city">City:</label> <input type="text" name="city" required> <br>
+        <label for="state">State:</label> <input type="text" name="state" required> <br>
+        <label for="zip">Zip Code:</label> <input type="text" name="zip" required> <br>
         <br>
-        <label for="email">Email:</label> <input type="email" name="email"> <br>
-        <label for="phone">Phone Number:</label> <input type="tel" name="phone"> <br>
+        <label for="email">Email:</label> <input type="email" name="email" required> <br>
+        <label for="phone">Phone Number:</label> <input type="tel" name="phone" required> <br>
 
         <!-- find other necessary info for registration -->
 
         <input type="submit">
     </form>
+    
 </body>
 </html>
--- src/functions/register.php
+++ src/functions/register.php
@@ -2,20 +2,20 @@
     require "db_functions.php";
     session_start();
     
-    $username = isset($_POST)?$_POST["username"]:"";
-    $password = isset($_POST)?$_POST["password"]:"";
-    $c_password = isset($_POST)?$_POST["c_password"]:"";
+    $username = isset($_POST)?$_POST["username"]:null;
+    $password = isset($_POST)?$_POST["password"]:null;
+    $c_password = isset($_POST)?$_POST["c_password"]:null;
 
-    $first_name = isset($_POST)?$_POST["first_name"]:"";
-    $last_name = isset($_POST)?$_POST["last_name"]:"";
+    $first_name = isset($_POST)?$_POST["first_name"]:null;
+    $last_name = isset($_POST)?$_POST["last_name"]:null;
 
-    $address = isset($_POST)?$_POST["address"]:"";
-    $city = isset($_POST)?$_POST["city"]:"";
-    $state = isset($_POST)?$_POST["state"]:"";
-    $zip = isset($_POST)?$_POST["zip"]:"";
+    $address = isset($_POST)?$_POST["address"]:null;
+    $city = isset($_POST)?$_POST["city"]:null;
+    $state = isset($_POST)?$_POST["state"]:null;
+    $zip = isset($_POST)?$_POST["zip"]:null;
     
-    $email = isset($_POST)?$_POST["email"]:"";
-    $phone = isset($_POST)?$_POST["phone"]:"";
+    $email = isset($_POST)?$_POST["email"]:null;
+    $phone = isset($_POST)?$_POST["phone"]:null;
     
     registerUser($username, $password, $c_password, $first_name, $last_name, $address, $city, $state, $zip, $email, $phone);
 
