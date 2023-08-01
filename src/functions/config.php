9719c707972084232caf089c5988c900977fe84a
Professor Reddig <111457409+reddign@users.noreply.github.com>
2023-08-01 11:45:02 -0400

    Update config.php
    fix

--- src/functions/config.php
+++ src/functions/config.php
@@ -1,6 +1,6 @@
 <?php
-$databasehost = "156.67.74.51";
-$database = "u413142534_fus";
-$databaseuser = "u413142534_spaces";
-$databasepassword = "WeR3ntSp@cez";
+$databasehost = "";
+$database = "";
+$databaseuser = "";
+$databasepassword = "";
 ?>
